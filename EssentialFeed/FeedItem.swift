41c2dcae367f05cf5ac358be693b191e8b0381f3
Amr Hossam <amrhossam96@hotmail.com>
2024-09-29 14:13:03 +0300

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Amr on 29/09/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

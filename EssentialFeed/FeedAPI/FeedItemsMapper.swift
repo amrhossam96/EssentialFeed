992054aaca326d10e61a14bd20efb4d9a52447da
Amr Hossam <amrhossam96@hotmail.com>
2024-09-30 13:28:03 +0300

    Move all mapping logic to the `FeedItemMapper`

--- EssentialFeed/FeedAPI/FeedItemsMapper.swift
+++ EssentialFeed/FeedAPI/FeedItemsMapper.swift
@@ -9,34 +9,37 @@ import Foundation
 
 internal final class FeedItemsMapper {
     
+    struct Root: Decodable {
+        let items: [Item]
+        var feed: [FeedItem] {
+            return items.map { $0.item }
+        }
+    }
+
+    struct Item: Equatable, Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL?
+        
+        var item: FeedItem {
+            FeedItem(
+                id: id,
+                description: description,
+                location: location,
+                imageURL: image
+            )
+        }
+    }
+    
     private static var OK_200: Int { return 200 }
     
-    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else {
+              return .failure(.invalidData)
+        }
+        return .success(root.feed)
         
-        struct Root: Decodable {
-            let items: [Item]
-        }
-
-        struct Item: Equatable, Decodable {
-            let id: UUID
-            let description: String?
-            let location: String?
-            let image: URL?
-            
-            var item: FeedItem {
-                FeedItem(
-                    id: id,
-                    description: description,
-                    location: location,
-                    imageURL: image
-                )
-            }
-        }
-        
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map { $0.item }
     }
 }

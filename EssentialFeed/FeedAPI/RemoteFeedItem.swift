aa60a85fed95e7cac3372920de0bf0d3ae839143
amrhossam96 <amrhossam96@hotmail.com>
2024-10-07 21:26:37 +0300

    Move RemoteFeedItem to its own file

---
+++ EssentialFeed/FeedAPI/RemoteFeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Amr on 07/10/2024.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Equatable, Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL?
+}

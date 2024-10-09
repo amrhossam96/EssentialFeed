e8542e36a84b59afb72f69cfd2cc465eb2fe29e6
amrhossam96 <amrhossam96@hotmail.com>
2024-10-09 16:03:11 +0300

    Load command fails on retrieval error

--- EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
@@ -20,6 +20,8 @@ class FeedStoreSpy: FeedStore {
     
     private var deletionCompletions: [DeletionCompletion] = []
     private var insertionCompletions: [InsertionCompletion] = []
+    private var retrievalCompletions: [RetrievalCompletion] = []
+
 
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
@@ -48,7 +50,12 @@ class FeedStoreSpy: FeedStore {
         insertionCompletions[index](nil)
     }
     
-    func retrieve() {
+    func retrieve(completion: @escaping RetrievalCompletion) {
         receivedMessages.append(.retrieve)
+        retrievalCompletions.append(completion)
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrievalCompletions[index](error)
     }
 }
--- EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -16,10 +16,22 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     
     func test_load_requestsCacheRetrieval() {
         let (sut, store) = makeSUT()
-        
-        sut.load()
-        
+        sut.load { _ in }
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failsOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        var receivedError: Error?
+        let retrievalError = anyNSError()
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeRetrieval(with: retrievalError)
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(receivedError as? NSError, retrievalError)
     }
     
     // MARK: - Helpers
@@ -30,5 +42,9 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(store, file: file, line: line)
         return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
 }

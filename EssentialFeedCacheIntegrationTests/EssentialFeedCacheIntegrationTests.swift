7c758426ad17f3a1aa96bff9a818595695999c1e
amrhossam96 <amrhossam96@hotmail.com>
2024-11-24 20:37:57 +0200

    Add EssentialFeedCacheintegrationTests' target to separate the potentially-slower cache integration tests from the fast unit/isolated tests]

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Amr on 24/11/2024.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+}

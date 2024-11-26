19c090ad59ae19c83646c5c8b090c287bed97b5c
amrhossam96 <amrhossam96@hotmail.com>
2024-11-26 15:31:25 +0200

    Add typealias for 'FeedStore.DeletionResult and "FeedStore. InsertionResult"

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -18,10 +18,16 @@ public struct CachedFeed {
 }
 
 public protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
+    
+    typealias DeletionResult = Error?
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Error?
+    typealias InsertionCompletion = (InsertionResult) -> Void
+    
     typealias RetrievalResult = Result<CachedFeed?, Error>
     typealias RetrievalCompletion = (RetrievalResult) -> Void
+    
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate theads if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
